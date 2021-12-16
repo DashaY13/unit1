3bcbed2dfe77e5794cb6f74180ea1ff4c4440bde
ДашаY13 <dasha130289@rambler.ru>
2021-12-16 15:02:17 +0300

    initial commit 1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,14 +7,53 @@ import static org.testng.Assert.assertEquals;
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
+    public void shouldMustAdd0() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 3000;
+        int amount = 50;
 
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 950;
 
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(actual, expected);
         System.out.println();
         System.out.println("Nothing needs to be added");
     }
