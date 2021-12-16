399b6746bb7a3e77a1c0ef756cd7dde6aad478b5
ДашаY13 <dasha130289@rambler.ru>
2021-12-16 14:39:00 +0300

    initial commit 1

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,12 +6,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
+
     @Test
-    void remain() {
+    public void shouldMustAdd0() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 3000;
+        int amount = 50;
+
         int actual = service.remain(amount);
-        int expected = 1000;
+        int expected = 950;
+
         assertEquals(expected, actual);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
+    }
+
+    @Test
+    public void shouldMustAdd3() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+        System.out.println();
+        System.out.println("Nothing needs to be added");
     }
 }
+
+
+
+
